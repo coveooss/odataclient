85977313c3c9015139be125a4a0e4c7b50a421be
Sebastien Belzile <sbelzile@coveo.com>
2018-02-13 06:45:24 -0500

    *missing important line

--- src/http/HttpRequestBuilder.ts
+++ src/http/HttpRequestBuilder.ts
@@ -63,6 +63,8 @@ export class HttpRequestBuilder implements IHttpRequestBuilder {
                     }
                 }
             }
+
+            xmlHttp.send(this.body);
         });
     }
 }
